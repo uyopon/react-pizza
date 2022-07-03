f85ef2922751465e0c963907f1851cbe77a8d796
boss <andryousha228@gmail.com>
2022-07-03 18:59:14 +0500

    import {Header, Categories} from

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css'
-import Header from './components/Header.jsx'
-
+import {Header, Categories} from './components'
 
 
 function App() {
@@ -12,16 +11,8 @@ function App() {
     <div className="content">
       <div className="container">
         <div className="content__top">
-          <div className="categories">
-            <ul>
-              <li className="active">Все</li>
-              <li>Мясные</li>
-              <li>Вегетарианская</li>
-              <li>Гриль</li>
-              <li>Острые</li>
-              <li>Закрытые</li>
-            </ul>
-          </div>
+          <Categories/>
+          
           <div className="sort">
             <div className="sort__label">
               <svg
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Button from './Button'
+import Categories from './categories';
+import Header from './Header.jsx'
+
+export {Button,Categories,Header}
