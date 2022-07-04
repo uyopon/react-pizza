8aedc15c45d5b2361d7a09e9f87a8b45eaa8e11f
boss <andryousha228@gmail.com>
2022-07-04 15:39:34 +0500

    onClick={() => { this.onSelectItem(index) }

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 
-function categories({ items }) {
+// function categories({ items }) {
 
-    return (
-        <div className="categories"> 
-            <ul>
-                <li className="active">Все</li>
-                {items.map(i => <li>{i}</li>)}
+//     return (
+//         <div className="categories"> 
+//             <ul>
+//                 <li className="active">Все</li>
+//                     {items.map((i, index) => <li key = {`${i}_${index}`} onClick= {()=>alert('')}>{i}</li>)}
 
-            </ul>
-        </div>
-    )
+//             </ul>
+//         </div>
+//     )
+// }
+
+// export default categories
+//rfce =)
+
+class categories extends React.Component {
+    state = {
+        activeItem: 0
+    }
+    onSelectItem = (index) => {
+        this.setState(////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            { activeItem: index }
+        )
+
+    }
+
+    render() {
+        const { items, onClickItem } = this.props
+        return (
+            <div className="categories">
+                <ul>
+
+                    {items.map((i, index) => <li className={this.state.activeItem === index ? "active" : ''} key={`${i}_${index}`} onClick={() => { this.onSelectItem(index) }}>{i}</li>)}
+                </ul>
+            </div>
+        )
+    }
+
 }
 
 export default categories
-//rfce =)
