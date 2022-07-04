7797ef4edc1b6bb991e7f63bd07a55c83254cd75
boss <andryousha228@gmail.com>
2022-07-04 13:15:54 +0500

    =)

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 
-function categories() {
-  return (
-    <div className="categories">
-    <ul>
-      <li className="active">Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
-    </ul>
-  </div>
-  )
+function categories({ items }) {
+
+    return (
+        <div className="categories"> 
+            <ul>
+                <li className="active">Все</li>
+                {items.map(i => <li>{i}</li>)}
+
+            </ul>
+        </div>
+    )
 }
 
 export default categories
-//rfce
+//rfce =)
