27181b6d525685efeaf70a6e48a40c8221817f04
boss <andryousha228@gmail.com>
2022-07-04 19:44:18 +0500

    const setActiveItem = state[1]

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,44 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// function categories({ items }) {
+function categories({ items }) {
 
-//     return (
-//         <div className="categories"> 
-//             <ul>
-//                 <li className="active">Все</li>
-//                     {items.map((i, index) => <li key = {`${i}_${index}`} onClick= {()=>alert('')}>{i}</li>)}
 
-//             </ul>
-//         </div>
-//     )
-// }
+    const state = React.useState(null)
+    const activeItem = state[0]
+    const setActiveItem = state[1]
+    console.log(state)
+    return (
+        <div className="categories">
+            <ul>
+                <li className="active">Все</li>
+                {items.map((i, index) => <li className={activeItem === index ? "active" : ''} key={`${i}_${index}`} onClick={() => { setActiveItem(index) }}>{i}</li>)}
 
-// export default categories
-//rfce =)
 
-class categories extends React.Component {
-    state = {
-        activeItem: 0
-    }
-    onSelectItem = (index) => {
-        this.setState(////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            { activeItem: index }
-        )
-
-    }
-
-    render() {
-        const { items, onClickItem } = this.props
-        return (
-            <div className="categories">
-                <ul>
-
-                    {items.map((i, index) => <li className={this.state.activeItem === index ? "active" : ''} key={`${i}_${index}`} onClick={() => { this.onSelectItem(index) }}>{i}</li>)}
-                </ul>
-            </div>
-        )
-    }
-
+            </ul>
+        </div>
+    )
 }
 
 export default categories
+//rfce =)
+
+
+
+// class categories extends React.Component {
+//     state = {
+//         activeItem: 0
+//     }
+//     onSelectItem = (index) => {
+//         this.setState(//ставит в очередь изменения состояния компонента и сообщает React, что этот компонент и его дочерние элементы необходимо повторно отобразить с обновленным состоянием.
+//             { activeItem: index }
+//         )
+
+//     }
+
+//     render() {
+//         const { items, onClickItem } = this.props
+//         return (
+//             <div className="categories">
+//                 <ul>
+
+//                     {items.map((i, index) => <li className={this.state.activeItem === index ? "active" : ''} key={`${i}_${index}`} onClick={() => { this.onSelectItem(index) }}>{i}</li>)}
+//                 </ul>
+//             </div>
+//         )
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
