233ba022b9bd5881ef87ac995278c6db00530d0e
boss <andryousha228@gmail.com>
2022-07-30 18:26:46 +0500

    ContentLoader

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import React from 'react'
 import PropTypes from 'prop-types'
+import ContentLoader from "react-content-loader"
 
 function PizzaBlock({ imageUrl, name, price, sizes, types }) {
    
@@ -20,6 +21,28 @@ function PizzaBlock({ imageUrl, name, price, sizes, types }) {
     }
 
     return (
+        
+        <React.Fragment>
+
+
+        {/* <ContentLoader 
+        speed={2}
+        width={288}
+        height={457}
+        viewBox="0 0 288 457"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+        
+      >
+        <circle cx="130" cy="130" r="130" /> 
+        <rect x="0" y="281" rx="10" ry="10" width="265" height="23" /> 
+        <rect x="0" y="319" rx="10" ry="10" width="268" height="60" /> 
+        <rect x="0" y="393" rx="19" ry="19" width="93" height="35" /> 
+        <rect x="124" y="392" rx="30" ry="30" width="143" height="49" /> 
+        <rect x="203" y="409" rx="0" ry="0" width="14" height="8" />
+      </ContentLoader> */}
+
+
         <div className="pizza-block">
             <img
                 className="pizza-block__image"
@@ -73,6 +96,7 @@ function PizzaBlock({ imageUrl, name, price, sizes, types }) {
 
 
         </div>
+        </React.Fragment>
     )
 }
 
